18f796a0292ab97ecf27b8c41861e2d1d78bff53
manelsen <emanuelmoura@gmail.com>
2026-02-25 14:52:38 -0300

    [API-9.2] Add API 9.2 module
    API 9.2 - Direct Messages in Channels (August 2025)
    - No new Bot API methods
    - Added: is_direct_messages field
    - Added: parent_chat field
    - Added: direct_messages_topic_id parameter

---
+++ src/api_versions/v92.jl
@@ -0,0 +1,16 @@
+"""
+    Telegram.API.V92
+
+Métodos da API 9.2 - Direct Messages in Channels
+Released: August 2025
+
+API 9.2 adds:
+- is_direct_messages field in Chat
+- parent_chat field in ChatFullInfo
+- direct_messages_topic_id parameter to send methods
+
+Note: No new Bot API methods in this version, mostly parameter additions.
+"""
+module V92
+# No new Bot API methods - parameter additions
+end
