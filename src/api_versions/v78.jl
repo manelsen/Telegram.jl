3d58a8af331fa650f7acb11bfd1968ebbd247d06
manelsen <emanuelmoura@gmail.com>
2026-02-25 14:40:50 -0300

    [API-7.8] Add API 7.8 module
    API 7.8 - Main Mini App (July 2024)
    - business_connection_id parameter added to pinChatMessage
    - business_connection_id parameter added to unpinChatMessage

---
+++ src/api_versions/v78.jl
@@ -0,0 +1,16 @@
+"""
+    Telegram.API.V78
+
+Métodos da API 7.8 - Main Mini App
+Released: July 2024
+
+API 7.8 adds:
+- business_connection_id parameter to pinChatMessage
+- business_connection_id parameter to unpinChatMessage
+
+These are implemented as parameters to existing methods.
+"""
+module V78
+# API 7.8 adds business_connection_id to pinChatMessage and unpinChatMessage
+# These are handled via kwargs in existing implementations
+end
