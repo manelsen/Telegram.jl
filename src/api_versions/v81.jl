bda2556185d3550553bc3da2d7f12e638997c8ca
manelsen <emanuelmoura@gmail.com>
2026-02-25 14:48:37 -0300

    [API-8.1] Add API 8.1 module
    API 8.1 - Affiliate Program (December 2024)
    - No new Bot API methods
    - Added: TransactionPartnerAffiliateProgram class
    - Added: AffiliateInfo class, affiliate field
    - Added: nanotar_amount field in StarTransaction

---
+++ src/api_versions/v81.jl
@@ -0,0 +1,19 @@
+"""
+    Telegram.API.V81
+
+Métodos da API 8.1 - Affiliate Program
+Released: December 2024
+
+API 8.1 adds:
+- TransactionPartnerAffiliateProgram class (for affiliate commissions)
+- AffiliateInfo class
+- Affiliate field in TransactionPartnerUser
+- nanotar_amount field in StarTransaction
+
+Note: No new Bot API methods, only new type definitions.
+"""
+module V81
+# No new Bot API methods in this version
+# New classes: TransactionPartnerAffiliateProgram, AffiliateInfo
+# New fields: nanotar_amount, affiliate
+end
