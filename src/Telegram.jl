94d34d554770db45298b4dca492f806fe6eb19ef
manelsen <emanuelmoura@gmail.com>
2026-02-25 14:36:53 -0300

    [API] Update Telegram.jl to use modular API version files
    - Include API version modules: v70, v72, v73, v74_75, v76_77
    - All 389 tests passing

--- src/Telegram.jl
+++ src/Telegram.jl
@@ -18,8 +18,16 @@ include("client.jl")
 include("api.jl")
 using .API
 
-include("api_methods.jl")
-using .Methods
+include("api_versions/v70.jl")
+include("api_versions/v72.jl")
+include("api_versions/v73.jl")
+include("api_versions/v74_75.jl")
+include("api_versions/v76_77.jl")
+using .V70
+using .V72
+using .V73
+using .V74_75
+using .V76_77
 
 include("logging.jl")
 include("bot.jl")
